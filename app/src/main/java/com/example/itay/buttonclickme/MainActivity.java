f87961d23750b0ea80b55088d2d4313579748cb5
Itay <aa0801@bs.amalnet.k12.il>
2018-11-29 22:35:12 +0200

    סיום תוכנית כפתור נלחץ

---
+++ app/src/main/java/com/example/itay/buttonclickme/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.itay.buttonclickme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.btn);
+    }
+
+    public void btn(View view) {
+    btn.setText("Oh I have been clicked ;)");
+    }
+}
